fa7b034ac0c3c766c81258fe0827ea0b91f11544
Anthony Li <anthonyrli1994@gmail.com>
2023-02-25 20:46:22 -0500

    modified publicpath

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = () => {
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
-      publicPath: './'
+      publicPath: 'https://online-text-editor.herokuapp.com/dist/'
     },
     plugins: [
       new HtmlWebpackPlugin({
